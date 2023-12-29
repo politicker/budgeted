4e38584123b4e1ab157f78b04211cec6cab957fa
Harrison Borges <hborges005@gmail.com>
2023-12-29 12:40:41 -0700

    support css modules

--- ui/esbuild.config.mjs
+++ ui/esbuild.config.mjs
@@ -1,7 +1,18 @@
 import * as esbuild from 'esbuild'
+import CssModulesPlugin from 'esbuild-css-modules-plugin'
 
 await esbuild.build({
 	entryPoints: ['src/renderer/index.tsx'],
 	bundle: true,
 	outfile: 'dist/bundle.js',
+	plugins: [
+		CssModulesPlugin({
+			// @see https://github.com/indooorsman/esbuild-css-modules-plugin/blob/main/index.d.ts for more details
+			force: true,
+			// emitDeclarationFile: true,
+			localsConvention: 'camelCaseOnly',
+			namedExports: true,
+			inject: false,
+		}),
+	],
 })
--- ui/src/renderer/App.tsx
+++ ui/src/renderer/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import styles from './App.module.css'
 
 export default function App() {
-	return <div>Hello, world. This is a React app!</div>
+	return <div className={styles.main}>Hello, world. This is a React app!</div>
 }
