57fdbf39ae0899f588cced6fb3135e9f99665055
Harrison Borges <hborges005@gmail.com>
2023-12-30 23:50:11 -0700

    use electron ipc hook

--- ui/src/renderer/App.tsx
+++ ui/src/renderer/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styles from './App.module.css'
+import useElectronIPC from './useElectronIpc'
 
 export default function App() {
-	return <div className={styles.main}>llo, woi orld. This is a React app!</div>
+	const data = useElectronIPC<string>('transactions')
+
+	return <div>{data && <p>Received data: {data}</p>}</div>
 }
