d625445954241efd5a4a652d9420c0594e2df05a
Harrison Borges <hborges005@gmail.com>
2024-01-03 01:03:48 -0700

    table styles sorta

--- ui/src/components/table/Table.tsx
+++ ui/src/components/table/Table.tsx
@@ -18,26 +18,37 @@ export default function TransactionsTable({
 	})
 
 	return (
-		<table>
-			<thead>
+		<table className="border border-gray-700 w-full text-left">
+			<thead className="bg-emerald-500">
 				<tr>
 					{table.getFlatHeaders().map((header) => (
-						<th key={header.id}>
+						<th key={header.id} className="capitalize px-3.5 py-2">
 							{flexRender(header.column.columnDef.header, header.getContext())}
 						</th>
 					))}
 				</tr>
 			</thead>
 			<tbody>
-				{table.getRowModel().rows.map((row) => (
-					<tr key={row.id}>
-						{row.getVisibleCells().map((cell) => (
-							<td key={cell.id}>
-								{flexRender(cell.column.columnDef.cell, cell.getContext())}
-							</td>
-						))}
+				{table.getRowModel().rows.length ? (
+					table.getRowModel().rows.map((row, i) => (
+						<tr
+							key={row.id}
+							className={`
+							${i % 2 === 0 ? 'bg-gray-700' : 'bg-gray-800'}
+						`}
+						>
+							{row.getVisibleCells().map((cell) => (
+								<td key={cell.id} className="px-3.5 py-2 text-white">
+									{flexRender(cell.column.columnDef.cell, cell.getContext())}
+								</td>
+							))}
+						</tr>
+					))
+				) : (
+					<tr className="text-center h-32">
+						<td colSpan={7}>No Records Found!</td>
 					</tr>
-				))}
+				)}
 			</tbody>
 		</table>
 	)
