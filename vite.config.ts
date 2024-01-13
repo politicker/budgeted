80d5152b22904f08f88383824f0326b8d62c9e13
quinn <q.shanahan@gmail.com>
2024-01-13 16:07:16 -0500

    build go bin into app

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,22 @@ import react from '@vitejs/plugin-react'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
 import pkg from './package.json'
+import { exec as execCallback } from 'node:child_process'
+import { promisify } from 'node:util'
+const exec = promisify(execCallback)
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(async ({ command }) => {
 	rmSync('dist-electron', { recursive: true, force: true })
 
 	const isServe = command === 'serve'
 	const isBuild = command === 'build'
 	const sourcemap = isServe || !!process.env.VSCODE_DEBUG
+
+	console.log('Building CLI...')
+	await exec('go build -o dist/budgeted-cli ./cmd', { cwd: './cli' })
+	await exec('cp cli/dist/budgeted-cli bin/budgeted-cli')
+	console.log('Finished building CLI.')
 
 	return {
 		resolve: {
