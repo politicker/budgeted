cf05c1309f012972281926ea9ccfc88e26a13744
quinn <q.shanahan@gmail.com>
2024-01-13 19:09:10 -0500

    import table from shadcn examples

--- src/components/TransactionsTable.tsx
+++ src/components/TransactionsTable.tsx
@@ -7,6 +7,9 @@ import { z } from 'zod'
 import { Updater } from '@tanstack/react-table'
 import { transactionColumns } from './columns'
 import { pick } from 'remeda'
+import { DataTable } from './ui/data-table/data-table'
+import tasks from './ui/data/tasks.json'
+import { columns } from './ui/data-table/columns'
 
 export type SetInputType = (
 	inp: Updater<Partial<z.infer<typeof FetchTransactionsInput>>>,
@@ -19,61 +22,69 @@ export type SetPaginationType = (
 ) => void
 
 export function TransactionsTable() {
-	const [input, setInputSimple] = useState<
-		z.infer<typeof FetchTransactionsInput>
-	>({
-		sort: 'desc',
-		sortColumn: 'date',
-		pageIndex: 0,
-		pageSize: 10,
-	})
-
-	const setInput: SetInputType = useCallback(
-		(inp) => {
-			// campitible with Updater
-			if (typeof inp === 'function') {
-				setInputSimple((prev) => ({ ...prev, ...inp(prev) }))
-				return
-			}
-
-			setInputSimple((prev) => ({ ...prev, ...inp }))
-		},
-		[setInputSimple],
-	)
-
-	const { data, refetch, isPreviousData } = trpc.transactions.useQuery(input, {
-		keepPreviousData: true,
-	})
-	const { mutate } = trpc.rebuildTransactions.useMutation({
-		onSuccess: () => refetch(),
-	})
-
 	return (
-		<>
-			<div className="p-3 border-b">
-				<Button variant="outline" onClick={() => mutate()}>
-					Rebuild Transactions
-				</Button>
-				&nbsp;&nbsp;
-				<Button
-					variant="outline"
-					onClick={() => {
-						setInput({ sort: input.sort === 'desc' ? 'asc' : 'desc' })
-					}}
-				>
-					Sorting: {input.sort}
-				</Button>
-			</div>
-
-			<Table
-				results={data?.results ?? []}
-				pageCount={data?.pageCount ?? 0}
-				pages={data?.pages ?? []}
-				setPagination={setInput as SetPaginationType}
-				setInput={setInput}
-				columns={transactionColumns}
-				pagination={pick(input, ['pageIndex', 'pageSize'])}
-			/>
-		</>
+		<div className="p-3">
+			<DataTable data={tasks} columns={columns} />
+		</div>
 	)
 }
+// export function TransactionsTable() {
+// 	const [input, setInputSimple] = useState<
+// 		z.infer<typeof FetchTransactionsInput>
+// 	>({
+// 		sort: 'desc',
+// 		sortColumn: 'date',
+// 		pageIndex: 0,
+// 		pageSize: 10,
+// 		showHidden: true,
+// 	})
+
+// 	const setInput: SetInputType = useCallback(
+// 		(inp) => {
+// 			// campitible with Updater
+// 			if (typeof inp === 'function') {
+// 				setInputSimple((prev) => ({ ...prev, ...inp(prev) }))
+// 				return
+// 			}
+
+// 			setInputSimple((prev) => ({ ...prev, ...inp }))
+// 		},
+// 		[setInputSimple],
+// 	)
+
+// 	const { data, refetch, isPreviousData } = trpc.transactions.useQuery(input, {
+// 		keepPreviousData: true,
+// 	})
+// 	const { mutate } = trpc.rebuildTransactions.useMutation({
+// 		onSuccess: () => refetch(),
+// 	})
+
+// 	return (
+// 		<>
+// 			<div className="p-3 border-b">
+// 				<Button variant="outline" onClick={() => mutate()}>
+// 					Rebuild Transactions
+// 				</Button>
+// 				&nbsp;&nbsp;
+// 				<Button
+// 					variant="outline"
+// 					onClick={() => {
+// 						setInput({ sort: input.sort === 'desc' ? 'asc' : 'desc' })
+// 					}}
+// 				>
+// 					Sorting: {input.sort}
+// 				</Button>
+// 			</div>
+
+// 			<Table
+// 				results={data?.results ?? []}
+// 				pageCount={data?.pageCount ?? 0}
+// 				pages={data?.pages ?? []}
+// 				setPagination={setInput as SetPaginationType}
+// 				setInput={setInput}
+// 				columns={transactionColumns}
+// 				pagination={pick(input, ['pageIndex', 'pageSize'])}
+// 			/>
+// 		</>
+// 	)
+// }
