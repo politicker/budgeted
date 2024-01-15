bda245f15a0d40b05edd936095c4c04b30bbde69
Harrison Borges <hborges005@gmail.com>
2024-01-14 19:47:23 -0500

    fix data table toolbar errors

--- src/components/ui/data-table/data-table-toolbar.tsx
+++ src/components/ui/data-table/data-table-toolbar.tsx
@@ -23,24 +23,24 @@ export function DataTableToolbar<TData>({
 		<div className="flex items-center justify-between m-3">
 			<div className="flex flex-1 items-center space-x-2">
 				<Input
-					placeholder="Filter tasks..."
-					value={(table.getColumn('title')?.getFilterValue() as string) ?? ''}
+					placeholder="Filter transactions..."
+					value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
 					onChange={(event) =>
-						table.getColumn('title')?.setFilterValue(event.target.value)
+						table.getColumn('name')?.setFilterValue(event.target.value)
 					}
 					className="h-8 w-[150px] lg:w-[250px]"
 				/>
-				{table.getColumn('status') && (
+				{/* {table.getColumn('merchantName') && (
 					<DataTableFacetedFilter
-						column={table.getColumn('status')}
-						title="Status"
+						column={table.getColumn('merchantName')}
+						title="Merchant"
 						options={statuses}
 					/>
 				)}
-				{table.getColumn('priority') && (
+				{table.getColumn('date') && (
 					<DataTableFacetedFilter
-						column={table.getColumn('priority')}
-						title="Priority"
+						column={table.getColumn('date')}
+						title="Date"
 						options={priorities}
 					/>
 				)}
@@ -53,7 +53,7 @@ export function DataTableToolbar<TData>({
 						Reset
 						<Cross2Icon className="ml-2 h-4 w-4" />
 					</Button>
-				)}
+				)} */}
 			</div>
 			<DataTableViewOptions table={table} />
 		</div>
