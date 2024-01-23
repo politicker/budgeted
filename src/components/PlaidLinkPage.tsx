76b68004667d59585cdab83e3742b50492eb3d75
Harrison Borges <hborges005@gmail.com>
2024-01-23 00:09:24 -0500

    use fancy shad form

--- src/components/PlaidLinkPage.tsx
+++ src/components/PlaidLinkPage.tsx
@@ -1,21 +1,21 @@
 import { trpc } from '@/lib/trpc'
 import { PlaidLinkOnSuccess, PlaidLink } from 'react-plaid-link'
 
-export function PlaidLinkPage() {
+export function PlaidLinkButton() {
 	const { data } = trpc.plaidLinkToken.useQuery()
 	const { mutate } = trpc.setPlaidPublicToken.useMutation({})
 
 	const onSuccess: PlaidLinkOnSuccess = function (publicToken) {
-		console.log('onsuccess plaid')
-		debugger
 		mutate(publicToken)
 	}
 
 	return (
 		data && (
-			<PlaidLink token={data} onSuccess={onSuccess}>
-				Link your bank account
-			</PlaidLink>
+			<div>
+				<PlaidLink token={data} onSuccess={onSuccess}>
+					Link your bank account
+				</PlaidLink>
+			</div>
 		)
 	)
 }
--- src/components/SettingsPage.tsx
+++ src/components/SettingsPage.tsx
@@ -1,38 +1,79 @@
 import { trpc } from '@/lib/trpc'
 import { Input } from './ui/input'
-import { PlaidLinkPage } from './PlaidLinkPage'
+import { PlaidLinkButton } from './PlaidLinkPage'
+import { Button } from './ui/button'
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from './ui/form'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 export function SettingsPage() {
 	const { mutate /*, refetch*/ } = trpc.createConfig.useMutation({})
+	const formSchema = z.object({
+		plaidClientId: z.string(),
+		plaidSecret: z.string(),
+	})
 
-	function onSubmit(e: React.FormEvent<HTMLFormElement>) {
-		e.preventDefault()
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+	})
 
-		const form = e.target as HTMLFormElement
-		const formData = new FormData(form)
-
-		const plaidClientId = formData.get('plaidClientId') as string
-		const plaidSecret = formData.get('plaidSecret') as string
-
+	function onSubmit(data: z.infer<typeof formSchema>) {
 		mutate({
-			plaidClientId,
-			plaidSecret,
+			plaidClientId: data.plaidClientId,
+			plaidSecret: data.plaidSecret,
 		})
 	}
 
 	return (
-		<>
-			<form onSubmit={onSubmit}>
-				<p>Plaid Client ID </p>
-				<Input type="text" name="plaidClientId" />
+		<div className="flex flex-col gap-12 p-8 w-1/2">
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+					<FormField
+						control={form.control}
+						name="plaidClientId"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Plaid Client ID</FormLabel>
+								<FormControl>
+									<Input placeholder="34534lkjjjkl345" {...field} />
+								</FormControl>
+								<FormDescription>
+									The Plaid Client ID from the devtools dashboard
+								</FormDescription>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="plaidSecret"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Plaid Secret</FormLabel>
+								<FormControl>
+									<Input placeholder="plaid-secret" {...field} />
+								</FormControl>
+								<FormDescription>
+									The Plaid Secret from the devtools dashboard
+								</FormDescription>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<Button type="submit">Submit</Button>
+				</form>
+			</Form>
 
-				<p>Plaid Secret </p>
-				<Input type="text" name="plaidSecret" />
-
-				<button type="submit">Submit</button>
-			</form>
-
-			<PlaidLinkPage />
-		</>
+			<PlaidLinkButton />
+		</div>
 	)
 }
