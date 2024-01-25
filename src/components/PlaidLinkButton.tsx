779545fbf924b055a317e2c95d81c1726de09073
Harrison Borges <hborges005@gmail.com>
2024-01-25 15:15:57 -0500

    dont re-link already linked accounts

--- src/components/PlaidLinkButton.tsx
+++ src/components/PlaidLinkButton.tsx
@@ -13,6 +13,27 @@ export function PlaidLinkButton() {
 			return
 		}
 
+		if (!data) {
+			return
+		}
+
+		// TODO: Do we need to dig into accounts too?
+		// I think we can push the user to the update flow
+		// if they try to link an account that's already linked.
+		// Maybe they'd do that if they want to add more bank accounts to an already-linked
+		// institution.
+
+		// Example: https://github.com/plaid/pattern/blob/master/server/routes/items.js#L41-L49
+		for (const inst of data.institutions) {
+			if (inst.plaidId === institution.institution_id) {
+				toast.error(
+					`Bank account already linked.
+						Use the Link update flow to re-authenticate an already-linked account`,
+				)
+				return
+			}
+		}
+
 		mutate({
 			publicToken,
 			institutionName: institution.name,
@@ -26,7 +47,7 @@ export function PlaidLinkButton() {
 		data && (
 			<div>
 				<PlaidLink
-					token={data}
+					token={data.token}
 					onSuccess={onSuccess}
 					style={{
 						padding: 'initial',
