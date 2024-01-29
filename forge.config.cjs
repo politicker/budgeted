669aee55493e0854aa301281bb08dd61977aa200
Harrison Borges <hborges005@gmail.com>
2024-01-28 22:41:40 -0500

    config code sign and notarize

--- forge.config.cjs
+++ forge.config.cjs
@@ -1,6 +1,13 @@
 module.exports = {
 	packagerConfig: {
 		asar: true,
+		osxSign: {},
+	},
+	osxNotarize: {
+		tool: 'notarytool',
+		appleId: process.env.APPLE_ID,
+		appleIdPassword: process.env.APPLE_PASSWORD,
+		teamId: process.env.APPLE_TEAM_ID,
 	},
 	rebuildConfig: {},
 	makers: [
